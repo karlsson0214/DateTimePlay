53b39887b2ad819a8841d108bd58130992b4e843
Karlsson <karlsson0214@gmail.com>
2019-11-03 17:52:44 +0100

    Add project files.

---
+++ DateTimePlay/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace DateTimePlay
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            DateTime date = DateTime.Now;
+            Console.WriteLine("Current date and time : " + date.ToString());
+            Console.WriteLine();
+
+            TimeSpan timeSpan = new TimeSpan(days: 7, hours: 1, minutes: 0, seconds: 0);
+            Console.WriteLine("Add this time span (Days.Hours.Minutes.Seconds): " + timeSpan.ToString());
+            Console.WriteLine();
+
+            date = date.Add(timeSpan);
+            Console.WriteLine("New date: " + date.ToString());
+            Console.WriteLine();
+
+        }
+    }
+}
